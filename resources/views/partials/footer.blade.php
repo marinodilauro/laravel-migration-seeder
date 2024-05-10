c0bd0e30815b69fa53bd8faaa7cc9c52aa44ea52
marinodilauro <m.dilauro90@gmail.com>
2024-05-10 14:14:39 +0200

    Add partial and layout files

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,50 @@
+<footer class="py-3 bg-dark text-white">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h3>Company</h3>
+        <ul class="list-unstyled">
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>Quick Links</h3>
+        <ul class="list-unstyled">
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>Legal</h3>
+        <ul class="list-unstyled">
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>Sitemap</h3>
+        <ul class="list-unstyled">
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+          <li><a href="">Lorem ipsum</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
